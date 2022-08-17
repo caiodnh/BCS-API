87c9d892aa8e2e758112062bbd93ffad7cd146d1
Seno <caiodnh@protonmail.com>
2022-08-17 08:13:55 -0230

    Add project files.

---
+++ Better Call Saul/Models/FavoriteCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Better_Call_Saul.Models
+{
+    public partial class FavoriteCharacter
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ Better Call Saul/Models/FavoriteEpisode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Better_Call_Saul.Models
+{
+    public partial class FavoriteEpisode
+    {
+        public int Id { get; set; }
+        public int? Season { get; set; }
+        public int? NumberWithInSeason { get; set; }
+        public string? Title { get; set; }
+    }
+}
