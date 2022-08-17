4b96cda3a510b88ec51221c41816520df898867c
Seno <caiodnh@protonmail.com>
2022-08-17 09:21:30 -0230

    Wrote services by hand

---
+++ Better Call Saul/Services/FavoriteCharactersService.cs
@@ -0,0 +1,106 @@
+﻿using Better_Call_Saul.Models;
+using System.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Better_Call_Saul.Services;
+
+public static class FavoriteCharactersService
+{
+    static readonly string constr = @"Data Source=localhost;Initial Catalog=BCS;User ID=seno;Password=123";
+    static FavoriteCharactersService()
+    {
+    }
+
+    public static List<FavoriteCharacter> GetAll()
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = "Select * from FavoriteCharacters";
+        SqlCommand cmd = new(sql, conn);
+        SqlDataReader dataReader = cmd.ExecuteReader();
+
+        List<FavoriteCharacter> characters = new();
+
+        while (dataReader.Read())
+        {
+            string fname = (string)dataReader.GetValue(1);
+            string sname = (string)dataReader.GetValue(2);
+            string gender = (string)dataReader.GetValue(3);
+            FavoriteCharacter c = new()
+            {
+                FirstName = fname,
+                LastName = sname
+            };
+
+            characters.Add(c);
+        }
+
+        conn.Close();
+        return characters;
+    }
+
+    public static FavoriteCharacter? Get(int id)
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"Select * from Characters where ID = {id}";
+        Console.WriteLine(sql);
+        SqlCommand cmd = new(sql, conn);
+        SqlDataReader dataReader = cmd.ExecuteReader();
+
+        if (dataReader.Read())
+        {
+            string fname = (string)dataReader.GetValue(1);
+            string sname = (string)dataReader.GetValue(2);
+            string gender = (string)dataReader.GetValue(3);
+            FavoriteCharacter c = new FavoriteCharacter
+            {
+                FirstName = fname,
+                LastName = sname
+            };
+
+            conn.Close();
+            return c;
+        }
+        else
+            return null;
+    }
+
+    public static void Add(FavoriteCharacter character)
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"INSERT INTO FavoriteCharacters VALUES ({character.FirstName},{character.LastName})";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public static void Delete(int id)
+    {
+        var character = Get(id);
+        if (character is null)
+            return;
+
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"delete from FavoriteCharacters where id = {id}";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public static void Update(FavoriteCharacter character)
+    {
+        var c = Get(character.Id);
+        if (c is null)
+            return;
+
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"update FavoriteCharacters set FirstName = {character.FirstName}, LastName = {character.LastName} where id = {character.Id}";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+}
---
+++ Better Call Saul/Services/FavoriteEpisodesService.cs
@@ -0,0 +1,109 @@
+﻿using Better_Call_Saul.Models;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Better_Call_Saul.Services;
+
+public static class FavoriteEpisodesService
+{
+    static readonly string constr = @"Data Source=localhost;Initial Catalog=BCS;User ID=seno;Password=123";
+    static FavoriteEpisodesService()
+    {
+    }
+
+    public static List<FavoriteEpisode> GetAll()
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = "Select * from FavoriteEpisodes";
+        SqlCommand cmd = new(sql, conn);
+        SqlDataReader dataReader = cmd.ExecuteReader();
+
+        List<FavoriteEpisode> Episodes = new();
+
+        while (dataReader.Read())
+        {
+            int season = (int)dataReader.GetValue(1);
+            int ep = (int)dataReader.GetValue(2);
+            string name = (string)dataReader.GetValue(3);
+            FavoriteEpisode c = new()
+            {
+                Season = season,
+                NumberWithInSeason = ep,
+                Title = name
+            };
+
+            Episodes.Add(c);
+        }
+
+        conn.Close();
+        return Episodes;
+    }
+
+    public static FavoriteEpisode? Get(int id)
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"Select * from Episodes where ID = {id}";
+        Console.WriteLine(sql);
+        SqlCommand cmd = new(sql, conn);
+        SqlDataReader dataReader = cmd.ExecuteReader();
+
+        if (dataReader.Read())
+        {
+            int season = (int)dataReader.GetValue(1);
+            int ep = (int)dataReader.GetValue(2);
+            string name = (string)dataReader.GetValue(3);
+            FavoriteEpisode c = new()
+            {
+                Season = season,
+                NumberWithInSeason = ep,
+                Title = name
+            };
+
+            conn.Close();
+            return c;
+        }
+        else
+            return null;
+    }
+
+    public static void Add(FavoriteEpisode episode)
+    {
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"INSERT INTO FavoriteEpisodes VALUES ({episode.Season},{episode.NumberWithInSeason},{episode.Title})";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public static void Delete(int id)
+    {
+        var Episode = Get(id);
+        if (Episode is null)
+            return;
+
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"delete from FavoriteEpisodes where id = {id}";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public static void Update(FavoriteEpisode episode)
+    {
+        var c = Get(episode.Id);
+        if (c is null)
+            return;
+
+        SqlConnection conn = new(constr);
+        conn.Open();
+        string sql = $"update FavoriteEpisodes set Season = {episode.Season}, NumberWithInSeason = {episode.NumberWithInSeason}, Title = {episode.Title} where id = {episode.Id}";
+        SqlCommand cmd = new(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+}
