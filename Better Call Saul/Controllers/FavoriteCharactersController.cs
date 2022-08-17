40c80486a69d91ab351b462ec8e014e6eddcdba2
Seno <caiodnh@protonmail.com>
2022-08-17 09:32:51 -0230

    Update controllers

--- Better Call Saul/Controllers/FavoriteCharactersController.cs
+++ Better Call Saul/Controllers/FavoriteCharactersController.cs
@@ -1,162 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Better_Call_Saul.Models;
+using Better_Call_Saul.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Better_Call_Saul.Models;
 
-namespace Better_Call_Saul.Controllers
+namespace BCS.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FavoriteCharacterController : ControllerBase
 {
-    public class FavoriteCharactersController : Controller
+    public FavoriteCharacterController()
     {
-        private readonly BCSContext _context;
-
-        public FavoriteCharactersController(BCSContext context)
-        {
-            _context = context;
-        }
-
-        // GET: FavoriteCharacters
-        public async Task<IActionResult> Index()
-        {
-              return _context.FavoriteCharacters != null ? 
-                          View(await _context.FavoriteCharacters.ToListAsync()) :
-                          Problem("Entity set 'BCSContext.FavoriteCharacters'  is null.");
-        }
-
-        // GET: FavoriteCharacters/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.FavoriteCharacters == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteCharacter = await _context.FavoriteCharacters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (favoriteCharacter == null)
-            {
-                return NotFound();
-            }
-
-            return View(favoriteCharacter);
-        }
-
-        // GET: FavoriteCharacters/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: FavoriteCharacters/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName")] FavoriteCharacter favoriteCharacter)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(favoriteCharacter);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(favoriteCharacter);
-        }
-
-        // GET: FavoriteCharacters/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.FavoriteCharacters == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteCharacter = await _context.FavoriteCharacters.FindAsync(id);
-            if (favoriteCharacter == null)
-            {
-                return NotFound();
-            }
-            return View(favoriteCharacter);
-        }
-
-        // POST: FavoriteCharacters/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName")] FavoriteCharacter favoriteCharacter)
-        {
-            if (id != favoriteCharacter.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(favoriteCharacter);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FavoriteCharacterExists(favoriteCharacter.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(favoriteCharacter);
-        }
-
-        // GET: FavoriteCharacters/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.FavoriteCharacters == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteCharacter = await _context.FavoriteCharacters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (favoriteCharacter == null)
-            {
-                return NotFound();
-            }
-
-            return View(favoriteCharacter);
-        }
-
-        // POST: FavoriteCharacters/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.FavoriteCharacters == null)
-            {
-                return Problem("Entity set 'BCSContext.FavoriteCharacters'  is null.");
-            }
-            var favoriteCharacter = await _context.FavoriteCharacters.FindAsync(id);
-            if (favoriteCharacter != null)
-            {
-                _context.FavoriteCharacters.Remove(favoriteCharacter);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FavoriteCharacterExists(int id)
-        {
-          return (_context.FavoriteCharacters?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<FavoriteCharacter>> GetAll() => FavoriteCharactersService.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<FavoriteCharacter> Get(int id)
+    {
+        var c = FavoriteCharactersService.Get(id);
+
+        if (c == null)
+            return NotFound();
+
+        return c;
+    }
+
+    // POST action
+    [HttpPost]
+    public IActionResult Create(FavoriteCharacter c)
+    {
+        FavoriteCharactersService.Add(c);
+        return CreatedAtAction(nameof(Create), new { id = c.Id }, c);
+    }
+
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, FavoriteCharacter c)
+    {
+        if (id != c.Id)
+            return BadRequest();
+
+        var existingChar = FavoriteCharactersService.Get(id);
+        if (existingChar is null)
+            return NotFound();
+
+        FavoriteCharactersService.Update(c);
+
+        return NoContent();
+    }
+
+    // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var c = FavoriteCharactersService.Get(id);
+
+        if (c is null)
+            return NotFound();
+
+        FavoriteCharactersService.Delete(id);
+
+        return NoContent();
+    } 
 }
--- Better Call Saul/Controllers/FavoriteEpisodesController.cs
+++ Better Call Saul/Controllers/FavoriteEpisodesController.cs
@@ -1,162 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Better_Call_Saul.Models;
+using Better_Call_Saul.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Better_Call_Saul.Models;
 
-namespace Better_Call_Saul.Controllers
+namespace BCS.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FavoriteEpisodeController : ControllerBase
 {
-    public class FavoriteEpisodesController : Controller
+    public FavoriteEpisodeController()
     {
-        private readonly BCSContext _context;
-
-        public FavoriteEpisodesController(BCSContext context)
-        {
-            _context = context;
-        }
-
-        // GET: FavoriteEpisodes
-        public async Task<IActionResult> Index()
-        {
-              return _context.FavoriteEpisodes != null ? 
-                          View(await _context.FavoriteEpisodes.ToListAsync()) :
-                          Problem("Entity set 'BCSContext.FavoriteEpisodes'  is null.");
-        }
-
-        // GET: FavoriteEpisodes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.FavoriteEpisodes == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteEpisode = await _context.FavoriteEpisodes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (favoriteEpisode == null)
-            {
-                return NotFound();
-            }
-
-            return View(favoriteEpisode);
-        }
-
-        // GET: FavoriteEpisodes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: FavoriteEpisodes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Season,NumberWithInSeason,Title")] FavoriteEpisode favoriteEpisode)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(favoriteEpisode);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(favoriteEpisode);
-        }
-
-        // GET: FavoriteEpisodes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.FavoriteEpisodes == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteEpisode = await _context.FavoriteEpisodes.FindAsync(id);
-            if (favoriteEpisode == null)
-            {
-                return NotFound();
-            }
-            return View(favoriteEpisode);
-        }
-
-        // POST: FavoriteEpisodes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Season,NumberWithInSeason,Title")] FavoriteEpisode favoriteEpisode)
-        {
-            if (id != favoriteEpisode.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(favoriteEpisode);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FavoriteEpisodeExists(favoriteEpisode.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(favoriteEpisode);
-        }
-
-        // GET: FavoriteEpisodes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.FavoriteEpisodes == null)
-            {
-                return NotFound();
-            }
-
-            var favoriteEpisode = await _context.FavoriteEpisodes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (favoriteEpisode == null)
-            {
-                return NotFound();
-            }
-
-            return View(favoriteEpisode);
-        }
-
-        // POST: FavoriteEpisodes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.FavoriteEpisodes == null)
-            {
-                return Problem("Entity set 'BCSContext.FavoriteEpisodes'  is null.");
-            }
-            var favoriteEpisode = await _context.FavoriteEpisodes.FindAsync(id);
-            if (favoriteEpisode != null)
-            {
-                _context.FavoriteEpisodes.Remove(favoriteEpisode);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FavoriteEpisodeExists(int id)
-        {
-          return (_context.FavoriteEpisodes?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
     }
+
+    // GET all action
+    [HttpGet]
+    public ActionResult<List<FavoriteEpisode>> GetAll() => FavoriteEpisodesService.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<FavoriteEpisode> Get(int id)
+    {
+        var c = FavoriteEpisodesService.Get(id);
+
+        if (c == null)
+            return NotFound();
+
+        return c;
+    }
+
+    // POST action
+    [HttpPost]
+    public IActionResult Create(FavoriteEpisode c)
+    {
+        FavoriteEpisodesService.Add(c);
+        return CreatedAtAction(nameof(Create), new { id = c.Id }, c);
+    }
+
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, FavoriteEpisode c)
+    {
+        if (id != c.Id)
+            return BadRequest();
+
+        var existingChar = FavoriteEpisodesService.Get(id);
+        if (existingChar is null)
+            return NotFound();
+
+        FavoriteEpisodesService.Update(c);
+
+        return NoContent();
+    }
+
+    // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var c = FavoriteEpisodesService.Get(id);
+
+        if (c is null)
+            return NotFound();
+
+        FavoriteEpisodesService.Delete(id);
+
+        return NoContent();
+    }   
 }
